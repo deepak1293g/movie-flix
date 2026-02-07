22e6b9d17373221128af732d19e7dd12d9ff3cbf
DG Mac <macbookpro@DG-MAC.local>
2026-02-07 10:31:44 +0530

    UI: Adjust BrowsePage grid to 3 columns per row

--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -53,11 +53,11 @@ const BrowsePage = () => {
 
                 {/* Content Grid */}
                 {loading ? (
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 sm:gap-8 pb-20">
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 pb-20">
                         {[...Array(12)].map((_, i) => <MovieCardSkeleton key={i} />)}
                     </div>
                 ) : content.length > 0 ? (
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 sm:gap-8 pb-20">
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 pb-20">
                         {content.map((item, index) => (
                             <div
                                 key={item._id}
